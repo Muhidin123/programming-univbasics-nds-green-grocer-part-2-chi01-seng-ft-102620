b37d3787c88fd42b0fda6350f32b831f9ab29da9
Muhidin Hukic <muhidinhukic1@gmail.com>
2020-10-14 20:37:17 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -1,25 +1,33 @@
 require_relative './part_1_solution.rb'
 
 def apply_coupons(cart, coupons)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This method **should** update cart
+  counter = 0
+  while counter < coupons.length
+    item = find_item_by_name_in_collection(coupons[counter][:item], cart)
+    coupon_item = "#{coupons[counter][:item]} W/COUPON"
+    cart_item_coupon = find_item_by_name_in_collection(coupon_item , cart)
+    if item && item[:count] >= coupons[counter][:num]
+      if cart_item_coupon
+        cart_item_coupon[:count] += coupons[counter][:num]
+        item[:count] -= coupons[counter][:num]
+      else
+        cart_item_coupon = {
+            :item => coupon_item,
+            :price => coupons[counter][:cost] / coupons[counter][:num],
+            :clearance => item[:clearance],
+            :count => coupons[counter][:num]
+        }
+        cart << cart_item_coupon
+        item[:count] -= coupons[counter][:num]
+      end
+    end
+    counter +=1
+  end
+  cart
 end
 
 def apply_clearance(cart)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This method **should** update cart
 end
 
 def checkout(cart, coupons)
-  # Consult README for inputs and outputs
-  #
-  # This method should call
-  # * consolidate_cart
-  # * apply_coupons
-  # * apply_clearance
-  #
-  # BEFORE it begins the work of calculating the total (or else you might have
-  # some irritated customers
 end
